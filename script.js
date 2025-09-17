0b4817721f855e70ec0c3cc226db30b4dbdd7f5e
mayankk1337 <ritiksingh2428@gmail.com>
2025-09-17 02:46:13 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name;
+	}
 
-class Student extends Person {}
+	set age(value){
+		if (value<0){
+			console.log("Age cannot be in negative");
+		}
+		this.age=value;
+	}
+	get age(){
+		return this.age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name}is studying`)
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name}is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
